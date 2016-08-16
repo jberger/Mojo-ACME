1ed2083feb7a0885f1a87a549fae5af3a3991022
Joel Berger <joel.a.berger@gmail.com>
2016-08-15 21:48:07 -0500

    update the agreement

--- lib/Mojolicious/Plugin/ACME.pm
+++ lib/Mojolicious/Plugin/ACME.pm
@@ -11,7 +11,7 @@ use Mojo::ACME::CA;
 
 my %authorities = (
   letsencrypt => {
-    agreement => 'https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf',
+    agreement => 'https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf',
     name => q[Let's Encrypt],
     intermediate => 'https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem',
     primary_url => Mojo::URL->new('https://acme-v01.api.letsencrypt.org'),
